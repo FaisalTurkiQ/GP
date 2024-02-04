85a973a7425d138e543b0cd5a4089755e8b4f766
FaisalAl-Qahtani <tech@iiitc.com>
2024-02-04 10:32:05 +0300

    add config/db.js

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 const express = require("express"); 
 require('dotenv').config();
-
 const app = express(); 
-
-const mongoose = require('mongoose');
-console.log(process.env.MONGODB_URI)
-
-mongoose.connect(process.env.MONGODB_URI).then(() =>{
-    app.listen(process.env.PORT, () => { 
-        console.log(`API is listening on port ${process.env.PORT}`); 
-    });
-    
-}).catch((err) => console.error(err));
+const db = require('./config/db');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -25,3 +15,7 @@ app.use("/chapters",chaptersRouter)
 
 const testsRouter = require("./routers/tests");
 app.use("/tests",testsRouter)
+
+app.listen(process.env.PORT, () => { 
+    console.log(`API is listening on port ${process.env.PORT}`); 
+});
